57ac8198eff551b81d11edf822883f0509f0214f
Johnny Winn <j.winn.v@gmail.com>
2017-08-28 20:43:13 -0600

    Include ExCoveralls for tet coverage statistics

--- mix.exs
+++ mix.exs
@@ -5,16 +5,20 @@ defmodule EwDashboards.Mixfile do
     [
       apps_path: "apps",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.html": :test],
+      aliases: aliases()
     ]
   end
 
-  # Dependencies listed here are available only for this
-  # project and cannot be accessed from applications inside
-  # the apps folder.
-  #
-  # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      {:excoveralls, "~> 0.7", only: :test}
+    ]
+  end
+
+  defp aliases do
+    ["coveralls": ["coveralls.detail -u"]]
   end
 end
