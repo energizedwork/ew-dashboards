67e12f5f6fdddf692ff34842f2ae737cf2e65338
msp <matt@cenatus.org>
2017-09-08 14:28:42 +0200

    Heroku DB config

---
+++ apps/core/config/prod.exs
@@ -0,0 +1,10 @@
+use Mix.Config
+
+config :core, Core.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
+# Do not print debug messages in production
+config :logger, level: :info
