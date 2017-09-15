1e5f3bc94a974c7afff9f60d1af5ded5ec856b71
msp <matt@cenatus.org>
2017-09-15 21:45:48 +0100

    Try fix the 403 when a client web socket connects..

--- apps/api/lib/api_web/channels/user_socket.ex
+++ apps/api/lib/api_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule ApiWeb.UserSocket do
   channel "dataSource:*", ApiWeb.DataSourceChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000, check_origin: false
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
