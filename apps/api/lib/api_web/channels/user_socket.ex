35d10c50281db569b26ff16ab5d44b844b91f9e3
msp <matt@cenatus.org>
2017-09-15 20:55:45 +0100

    Heroku config for the API

--- apps/api/lib/api_web/channels/user_socket.ex
+++ apps/api/lib/api_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule ApiWeb.UserSocket do
   channel "dataSource:*", ApiWeb.DataSourceChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
