dcb21c8a5b5800cc59fd99d6853d1a3677469a5e
msp <matt@cenatus.org>
2017-09-15 20:43:26 +0100

    Use ENV vars on Heroku

--- apps/api/config/prod.exs
+++ apps/api/config/prod.exs
@@ -58,7 +58,3 @@ config :logger, level: :info
 #
 #     config :api, ApiWeb.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
