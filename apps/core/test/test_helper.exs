95472e7d29d483e6230678c59d5baf8673cb3af9
msp <matt@cenatus.org>
2017-09-18 16:48:52 +0100

    Don't create the DB on Heroku

--- apps/core/test/test_helper.exs
+++ apps/core/test/test_helper.exs
@@ -1,4 +1,7 @@
 ExUnit.start()
 
-Mix.Task.run "ecto.create", ["--quiet"]
+unless System.get_env("DATABASE_URL") do
+  Mix.Task.run "ecto.create", ["--quiet"]
+end
+
 Mix.Task.run "ecto.migrate", ["--quiet"]
